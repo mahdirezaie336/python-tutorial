046850a7e648b7b9348669a0305a64c9c1dad632
mahdirezaie336 <mahdi.rezaie.336@gmail.com>
2020-12-22 10:36:17 +0330

    Read and write file using 'with as'

--- File/file_test.py
+++ File/file_test.py
@@ -1,8 +1,6 @@
-file = open('./sample.txt', 'r+')
-
-file.truncate(0)
-file.write('Hello from the children of planet Earth\n')
-file.flush()
-file.seek(0)
-print(file.readline())
-file.close()
+with open('./sample.txt', 'r+') as file:
+    file.truncate(0)
+    file.write('Hello from all of the children of planet Earth\n')
+    file.flush()
+    file.seek(0)
+    print(file.readline())
