8cfa3190df58fa84aaf0692cb00695f9fafbf093
mahdirezaie336 <mahdi.rezaie.336@gmail.com>
2020-12-30 14:10:26 +0330

    Create a list of input

--- DataStructures/music_suggestion.py
+++ DataStructures/music_suggestion.py
@@ -1,4 +1,15 @@
-users = set()
-albums = set()
+users = {}
+albums = {}
 
+n = int(input())
+inputs = []
+line = ''
+while not (line := input()).isdecimal():
+    inputs.append(line)
 
+index = 0
+for i in range(n):
+    name = inputs[index].split()[2]
+    index = index + 1
+    age = inputs[index]
+    
