6c36219d1f8694dde7726963a6948c87adf9a87f
mahdirezaie336 <mahdi.rezaie.336@gmail.com>
2021-01-25 15:40:39 +0330

    Finish proxy practice

--- OOP2/Proxy.py
+++ OOP2/Proxy.py
@@ -1,15 +1,36 @@
 class Proxy:
+
     def __init__(self, obj):
-        self._obj = obj
+        self.obj = obj
+        self.attr_dict = {}
+        self.last_invoked = None
+
+    def __getattr__(self, item):
+        if item not in dir(self.obj):
+            raise Exception('No Such Method')
+
+        if item not in self.attr_dict.keys():
+            self.attr_dict[item] = 0
+        self.attr_dict[item] += 1
+        self.last_invoked = item
+
+        return getattr(self.obj, item)
 
     def last_invoked_method(self):
-        pass
+        if self.last_invoked is None:
+            raise Exception('No Method Is Invoked')
+        return self.last_invoked
 
     def count_of_calls(self, method_name):
-        pass
+        if method_name not in self.attr_dict.keys():
+            return 0
+        return self.attr_dict[method_name]
 
     def was_called(self, method_name):
-        pass
+        if method_name not in self.attr_dict.keys():
+            return False
+        return True
+
 
 class Radio():
     def __init__(self):
