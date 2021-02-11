0fc6649634f1d1d8ca83446d5cf6291cc5f076cc
mahdirezaie336 <mahdi.rezaie.336@gmail.com>
2021-02-11 21:08:46 +0330

    Finish practice 4

--- Reviewing Practices 2/4/security.py
+++ Reviewing Practices 2/4/security.py
@@ -4,18 +4,12 @@ import re
 class Security:
 
     def secure(self, info):
-        result = []
         parts = re.split('[\s,]+', info)
         for part in parts:
-            print(part)
             if self.is_social_account_info(part):
                 p = part.split('/')
-                result.append(p[0] + '/' + self.encrypt(p[1]))
-            else:
-                result.append(part)
-            result.append(', ')
-        del result[-1]
-        return ''.join(result)
+                info = info.replace(part, ''.join([p[0], '/', self.encrypt(p[1])]))
+        return info
 
     def is_social_account_info(self, param):
         r = re.match('[A-Z][a-z]*:www.[a-z]+.[a-z]+/[\w_]+', param)
@@ -43,8 +37,3 @@ class Security:
         i += 1
         parts.append(s[j:i])
         return parts
-
-
-ss = Security()
-print(ss.secure('FirstName:Ali, LastName:Alavi, BirthDate:1990/02/02 Gender:male Instagram:www.instagram.com/aalavi Degree:Master Twitter:www.twiter.com/alaviii imdb:www.imdb.com/alavi'))
-print(ss.is_social_account_info('Instagram:www.instagram.com/aalavi'))
