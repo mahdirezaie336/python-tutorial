0e610c38d704b5a9d1bf741f3ab2f9b62b1d592c
mahdirezaie336 <mahdi.rezaie.336@gmail.com>
2021-02-12 12:22:21 +0330

    Finish delete and restore in practice 5

--- Reviewing Practices 2/5/source.py
+++ Reviewing Practices 2/5/source.py
@@ -1,8 +1,20 @@
 import os
 
 
-class FileManager:
+class File:
 
+    def __init__(self, file_name, file_address):
+        self.name = file_name
+        self.address = file_address
+
+    def __eq__(self, other):
+        return (self.name == other.name) and (self.address == other.address)
+
+    def __hash__(self):
+        return hash(self.address + '/' + self.name)
+
+
+class FileManager:
     trash_dir = './.trash/'
 
     def __init__(self):
@@ -24,13 +36,19 @@ class FileManager:
     def delete(self, name, address):
         path = os.path.join(address, name)
         if os.path.exists(path):
-            os.rename(path, os.path.join(FileManager.trash_dir, name))
-            self.trashed_origins[name] = path
+            if name not in self.trashed_origins:
+                self.trashed_origins[name] = []
+            index = len(self.trashed_origins[name])
+            os.rename(path, os.path.join(FileManager.trash_dir, name + '.' + str(index)))
+            self.trashed_origins[name].append(path)
 
     def find(self, name, address):
         pass
 
     def restore(self, name):
-        path = os.path.join(FileManager.trash_dir, name)
-        if os.path.exists(path):
-            os.rename(path, self.trashed_origins.pop(name))
+        if name in self.trashed_origins:
+            origin = self.trashed_origins[name].pop()
+            index = len(self.trashed_origins[name])
+            os.rename(os.path.join(FileManager.trash_dir, name + '.' + str(index)), origin)
+            if index == 0:
+                del self.trashed_origins[name]
